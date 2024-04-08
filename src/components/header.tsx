e0441fa6724914b173f20650cd279373b8dd1061
Gabriel <gabrieldbronisio@gmail.com>
2024-04-08 17:48:38 -0300

    🪛feat: header implementation

---
+++ src/components/header.tsx
@@ -0,0 +1,62 @@
+"use client"
+import Image from "next/image";
+import { styled } from "styled-components"
+import { PrimaryInputWSearchIcon } from "./primary-input";
+
+interface HeaderProps{
+}
+
+
+const HeaderBase = styled.header`
+    display: flex;
+    justify-content: center; 
+    align-items: center;
+    flex-direction: column;
+    width: 100%; 
+    max-width: 1440px;
+    margin: 0 auto; 
+`;
+
+
+const TagHeader = styled.div`
+  padding: 14px 24px;
+  width: 100%;
+  background: linear-gradient(272deg, #ae276f 0%, #5f1478 100%);
+  box-shadow: 0px 3px 6px #00000029;
+  opacity: 1;
+`;
+
+
+
+const BackgroundImage = styled.div`
+  position: relative;
+  width: 100%;
+  height: 261px;
+  background-image: url("/fundo-busca@2x.png");
+  background-size: cover;
+  background-position: center;
+`;
+
+const StyledImage = styled(Image)`
+  background: 0% 0% no-repeat padding-box;
+  opacity: 1 ;
+`;
+
+export function Header(props : HeaderProps){
+    return(
+        <HeaderBase>
+           <TagHeader>
+            <StyledImage
+              src="/logo-teste.svg"
+              alt="Logo"
+              width={192}
+              height={37}
+            />
+           </TagHeader>
+           <BackgroundImage>
+             <PrimaryInputWSearchIcon  type="text" placeholder="Digite aqui sua busca..." />
+           </BackgroundImage>
+        </HeaderBase>
+        
+    )
+}
---
+++ src/components/primary-input.tsx
@@ -0,0 +1,49 @@
+import { InputHTMLAttributes } from "react";
+import { styled } from "styled-components";
+
+
+const InputContainer = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 1046px;
+  margin: 147px auto 0; /* Ajusta a margem para posicionar o input mais para baixo */
+`;
+
+export const PrimaryInput  = styled.input`
+  width: 100%;
+  height: 75px;
+  padding: 0 16px;
+  background: #FFFFFF;
+  border: none;
+  border-radius: 8px;
+  opacity: 1;
+  font-family: inherit;
+  font-weight: 400;
+  font-size: 24px;
+  line-height:30px;
+  letter-spacing: 0px;
+  color: #757575; 
+  box-shadow: 0px 3px 6px #00000029; 
+`;
+
+const MagnifyingGlass = styled.img`
+  position: absolute;
+  right: 16px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+`;
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+}
+
+export function PrimaryInputWSearchIcon(props: InputProps){
+    return (
+      <InputContainer>
+        <PrimaryInput 
+          {...props}
+        />
+        <MagnifyingGlass src="/lupa.svg" alt="Lupa" />
+      </InputContainer>
+    )
+  }
